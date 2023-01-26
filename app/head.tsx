7ca4199af040f89eb04e758aaab2132bdede7a06
İsmail Yarar <ismailyarar4@gmail.com>
2023-01-26 23:03:16 +0300

    Add google tag manager

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 export default function Head() {
   return (
     <>
@@ -33,6 +35,18 @@ export default function Head() {
       <meta property="twitter:image" content="/logo.png" />
       <link rel="canonical" href="https://kvarno.netlify.app/" />
       <link rel="manifest" href="/manifest.json" />
+      <Script
+        id="google-tag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P7J4MD8');`,
+        }}
+      ></Script>
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,14 @@ export default function RootLayout({
       <body className="overflow-y-scroll bg-gray-1100 p-4 text-white antialiased">
         <Header />
         {children}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7J4MD8"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
