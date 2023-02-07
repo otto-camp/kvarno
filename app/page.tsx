71751be3e7d496614785a847c0c80caf2947af66
İsmail Yarar <ismailyarar4@gmail.com>
2023-02-07 14:50:43 +0300

    Add gradient to homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
       <h1 className="mb-8 text-center text-4xl font-bold sm:text-5xl md:text-6xl lg:text-8xl">
         Discover the ultimate CSS and HTML tools
       </h1>
-      <section className="mx-auto mt-6 flex flex-col justify-center gap-6 md:mt-12">
+      <section className="relative mx-auto mt-6 flex flex-col justify-center gap-6 md:mt-12">
         <div className="flex flex-col items-start gap-4 text-3xl font-semibold text-white">
           <h2 className="">Create Gradient</h2>
           <p className="text-lg font-normal">
@@ -51,6 +51,32 @@ export default function Page() {
           </a>
         </div>
       </section>
+      <div className="absolute inset-x-0 top-0 -z-10 transform-gpu overflow-hidden blur-3xl">
+        <svg
+          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC" />
+              <stop offset={1} stopColor="#FF80B5" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
     </main>
   );
 }
